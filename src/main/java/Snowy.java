2f4cea22117641880e099c952126bbc9fbdebb59
minwaiphyo <minwaiphyo@gmail.com>
2026-01-21 21:25:52 +0800

    Add Snowy echo feature

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class Snowy {
     public static void main(String[] args) {
 
-        // Greets user and exit
+        Scanner scanner = new Scanner(System.in);
+
+        // Greets user
         System.out.println("""
                 ____________________________________________________________
                 Woof woof! I'm Snowy!
-                What can I do for you?
+                  /^-----^\\
+                 V  o o  |\s
+                  |  Y  | \s
+                   \\ Q /  \s
+                   / - \\  \s
+                   |    \\
+                   |     \\     )
+                   || (___\\====
+                What can I do for you? :3
                 ____________________________________________________________
-                Bye. Hope to see you again soon!
-                ____________________________________________________________
+                
                 """);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            // User terminating
+            if (input.equals("bye")) {
+                System.out.println("     _______________________________________________________");
+                System.out.println("     *Sad puppy noises* Bye... Hope to play with you again soon!");
+                System.out.println("     _______________________________________________________");
+                return;
+            }
+
+            // Echoes back user input
+            System.out.println("     _______________________________________________________");
+            System.out.println("     " + input);
+            System.out.println("     _______________________________________________________");
+        }
+
     }
+
 }
