679eef38db85cdd387dc497a68577b54aafbc36e
minwaiphyo <minwaiphyo@gmail.com>
2026-01-21 21:49:05 +0800

    Add Snowy add, list feature

--- src/main/java/Snowy.java
+++ src/main/java/Snowy.java
@@ -4,6 +4,9 @@ public class Snowy {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
 
         // Greets user
         System.out.println("""
@@ -25,20 +28,33 @@ public class Snowy {
         while (true) {
             String input = scanner.nextLine();
 
+
             // User terminating
             if (input.equals("bye")) {
                 System.out.println("     _______________________________________________________");
                 System.out.println("     *Sad puppy noises* Bye... Hope to play with you again soon!");
                 System.out.println("     _______________________________________________________");
-                return;
+                break;
             }
 
-            // Echoes back user input
-            System.out.println("     _______________________________________________________");
-            System.out.println("     " + input);
-            System.out.println("     _______________________________________________________");
+            // Lists out tasks
+            if (input.equals("list")) {
+                System.out.println("     _______________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("     _______________________________________________________");
+            } else {
+                // Adds user input to tasks
+                tasks[taskCount] = input;
+                taskCount++;
+
+                // Confirms addition of new task
+                System.out.println("     _______________________________________________________");
+                System.out.println("     " + "added: " +  input);
+                System.out.println("     _______________________________________________________");
+            }
         }
-
+        scanner.close();
     }
-
 }
