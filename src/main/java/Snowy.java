5e92d9bd17ab9efd4145c9918cbcd440e3511de8
minwaiphyo <minwaiphyo@gmail.com>
2026-01-21 20:54:19 +0800

    Add Snowy greet and exit feature

---
+++ src/main/java/Snowy.java
@@ -0,0 +1,14 @@
+public class Snowy {
+    public static void main(String[] args) {
+
+        // Greets user and exit
+        System.out.println("""
+                ____________________________________________________________
+                Woof woof! I'm Snowy!
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """);
+    }
+}
