8b5388a9c91d53d035c19c2a45a4ea356ccf8c82
minwaiphyo <minwaiphyo@gmail.com>
2026-02-09 12:34:40 +0800

    Fix bug

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -9,7 +9,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-"C:\Users\Min Wai Phyo\.jdks\ms-17.0.17\bin\javac"  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -17,7 +17,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-"C:\Users\Min Wai Phyo\.jdks\ms-17.0.17\bin\java" -classpath ..\bin snowy.Snowy < input.txt > ACTUAL.TXT
+javac -classpath ..\bin snowy.Snowy < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
