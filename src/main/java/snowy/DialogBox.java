fc669d06448cb45c3810913a333e2d754be62f0c
minwaiphyo <minwaiphyo@gmail.com>
2026-02-16 17:51:57 +0800

    Improve GUI

--- src/main/java/snowy/DialogBox.java
+++ src/main/java/snowy/DialogBox.java
@@ -88,5 +88,6 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 }
